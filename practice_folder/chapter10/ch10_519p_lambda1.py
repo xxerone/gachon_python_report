b02b609cd49e361cc94ab3ef9f59edc2bad30b30
xxerone <167948652+xxerone@users.noreply.github.com>
2024-05-21 15:12:11 +0900

    Update ch10_519p_lambda1.py

--- practice_folder/chapter10/ch10_519p_lambda1.py
+++ practice_folder/chapter10/ch10_519p_lambda1.py
@@ -1,15 +1,5 @@
 #직접 타자 안침 주의!! / 슬라이드 21 / 519p lambda1.py
 
-class Student:
-        def __init__(self, name, grade, number):
-                self.name = name
-                self.grade = grade
-                self.number = number
-        def __repr__(self):
-                return repr((self.name, self.grade, self.number))
- students = [
-        Student('홍길동', 3.9, 20160303),
-        Student('김철수', 3.0, 20160302),
-        Student('최자영', 4.3, 20160301),
- ]
- print(sorted(students, key=lambda student: student.number)  
+f_temp = [0, 10, 20, 30, 40, 50]
+c_temp = map(lambda x: (5.0/9.0)*(x-32.0), f_temp)
+print(list(c_temp))
