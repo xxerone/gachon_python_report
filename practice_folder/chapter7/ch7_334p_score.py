d05df78283f95d308e8aa871add24ef7d5991720
xxerone <167948652+xxerone@users.noreply.github.com>
2024-06-20 20:23:41 +0900

    Create ch7_334p_score.py

---
+++ practice_folder/chapter7/ch7_334p_score.py
@@ -0,0 +1,5 @@
+from collections import Counter
+text_data ="Create the highest, grandest vision possible for your life, because 
+you become what you believe"
+a = Counter(text_data.split())
+print(a)
